205e0305450c8c36c30cabc65e215b28e18263e1
Peinoi <124995489+Peinoi@users.noreply.github.com>
2026-04-26 16:21:18 +0900

    first commit

---
+++ src/main/kotlin/com/kr/Krproject01Application.kt
@@ -0,0 +1,11 @@
+package com.kr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Krproject01Application
+
+fun main(args: Array<String>) {
+    runApplication<Krproject01Application>(*args)
+}
---
+++ src/test/kotlin/com/kr/Krproject01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Krproject01ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
