67b545b7dcbc5f78222e35e6884c3858efb9a5fb
Peinoi <124995489+Peinoi@users.noreply.github.com>
2026-04-29 14:57:50 +0900

    design:화면 세팅

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -4,15 +4,18 @@ import styles from './DashboardPage.module.css'
 export default function DashboardPage() {
     return (
         <div className={styles.simpleWrapper}>
+                <h1 className={styles.title}>대쉬 보드</h1>
             <div className={styles.container}>
-                <h2 className={styles.title}>유저 목록</h2>
                 <div className={styles.listCard}>
+                    <h2>유저 목록</h2>
                     <UserList />
                 </div>
                 <div className={styles.listCard}>
+                    <h2>게시판</h2>
                     <UserList />
                 </div>
                 <div className={styles.listCard}>
+                    <h2>차트</h2>
                     <UserList />
                 </div>
             </div>
